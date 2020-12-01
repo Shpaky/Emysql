c1bc1bd4b11d06450e7e014db78f8788c2f3377f
Shpaky <vladimirsolenkov@yandex.ru>
2020-12-01 18:43:25 +0300

    fix - 'asciiz/1' function of 'emysql_auth' module was fixed

--- src/emysql_auth.erl
+++ src/emysql_auth.erl
@@ -200,5 +200,7 @@ hash([], N1, N2, _Add) ->
     {N1 band Mask , N2 band Mask}.
 
 asciiz(Data) when is_binary(Data) ->
-    [S, R] = binary:split(Data, <<0>>),
-    {S, R}.
+    case binary:split(Data, <<0>>) of
+        [S1, R] -> {S1, R};
+        [S2] -> {S2, <<>>}
+    end.
